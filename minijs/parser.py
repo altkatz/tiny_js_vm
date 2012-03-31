339a4d42c1c5819e10b636ea2a3f26a12a301eeb
Alex Gaynor <alex.gaynor@gmail.com>
2012-03-31 13:27:53 -0400

    Working parser!

--- minijs/parser.py
+++ minijs/parser.py
@@ -1,9 +1,10 @@
 import os
 
-from pypy.rlib.parsing.ebnfparse import parse_ebnf
+from pypy.rlib.parsing.ebnfparse import parse_ebnf, make_parse_function
 
 
-grammar = os.path.join(os.path.dirname(__file__), "grammar.txt")
+with open(os.path.join(os.path.dirname(__file__), "grammar.txt")) as f:
+    grammar = f.read()
 regexs, rules, _ = parse_ebnf(grammar)
 _parse = make_parse_function(regexs, rules, eof=True)
 
@@ -28,4 +29,42 @@ class ConstantFloat(Node):
 
 
 class Transformer(object):
-    pass
+    def visit_main(self, node):
+        return self.visit_block(node.children[0])
+
+    def visit_block(self, node):
+        stmts = []
+        while True:
+            stmts.append(self.visit_stmt(node.children[0]))
+            if len(node.children) == 1:
+                break
+            node = node.children[1]
+        return Block(stmts)
+
+    def visit_stmt(self, node):
+        if node.children[0].symbol == "expr":
+            return Stmt(self.visit_expr(node.children[0]))
+        raise NotImplementedError
+
+    def visit_expr(self, node):
+        if node.symbol == "expr":
+            node = node.children[0]
+        if node.symbol == "comparison":
+            return self.visit_subexpr(node)
+        elif node.symbol == "primary":
+            return self.visit_primary(node)
+        raise NotImplementedError
+
+    def visit_subexpr(self, node):
+        if len(node.children) == 1:
+            return self.visit_expr(node.children[0])
+        raise NotImplementedError
+
+    def visit_primary(self, node):
+        if len(node.children) == 1:
+            return self.visit_atom(node.children[0])
+        raise NotImplementedError
+
+    def visit_atom(self, node):
+        if node.children[0].symbol == "FLOAT":
+            return ConstantFloat(float(node.children[0].additional_info))
--- tests/test_parser.py
+++ tests/test_parser.py
@@ -3,4 +3,4 @@ from minijs.parser import Block, Stmt, ConstantFloat
 
 class TestParser(object):
     def test_const_float(self, space):
-        assert space.parse("1") == Block([Stmt(ConstantFloat(1))])
+        assert space.parse("1;") == Block([Stmt(ConstantFloat(1))])
