abf343c31fb2ac9e58bc123deabf9660fdb6248e
Alex Gaynor <alex.gaynor@gmail.com>
2012-03-31 20:32:20 -0400

    play nice with coverage testing

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
-from minijs.objspace import ObjectSpace
-
-
 def pytest_funcarg__space(request):
+    # Import is inside the function so that it executes after coverage is setup,
+    # if you're using pytest-cov.
+    from minijs.objspace import ObjectSpace
+
     options = getattr(request.cls, "space_options", {})
     return ObjectSpace(**options)
